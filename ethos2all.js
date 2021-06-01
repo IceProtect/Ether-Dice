66babd789d38c63c5d1be37080a968a8a4cb6b6f
IceProtect <84831290+IceProtect@users.noreply.github.com>
2021-06-01 14:57:02 -0700

    Update ethos2all.js

--- ethos2all.js
+++ ethos2all.js
@@ -1,84 +1,15 @@
-function ethos2eth(ethos) {
-    return ethos * 0.000001
-}
-function getEthos() {
-Array.prototype.forEach.call(document.getElementsByClassName("balance-container nav-item"), async function(balance) {
-    if(balance.innerText.indexOf('ETHOS') != -1){
-    var ethos = balance.innerText.split("ETHOS")[1];
-    console.log(ethos)
-    return ethos;
-}
+fetch("https://raw.githubusercontent.com/IceProtect/Ether-Dice/main/ethos2cad.js").then(resp=>{
+resp.text().then(code=>{
+eval(code)
 })
-}
-function draw_boxCA() {
-var box = "<div class='balance-container nav-item' id='caddiv'><span>CAD</span><span id='cad'>$0.00</span></div>"
-var navbar = document.getElementsByClassName("navbar-collapse collapse")[0]
-navbar.innerHTML = box + navbar.innerHTML
-}
-function draw_boxETH() {
-var box = "<div class='balance-container nav-item' id='ethdiv'><span>ETH</span><span id='eth'>0.00</span></div>"
-var navbar = document.getElementsByClassName("navbar-collapse collapse")[0]
-navbar.innerHTML = box + navbar.innerHTML
-}
-function draw_boxUS() {
-var box = "<div class='balance-container nav-item' id='usddiv'><span>USD</span><span id='usd'>$0.00</span></div>"
-var navbar = document.getElementsByClassName("navbar-collapse collapse")[0]
-navbar.innerHTML = box + navbar.innerHTML
-}
-async function start() {
-    console.log("Made by Titan")
-    if(document.getElementById("ethdiv")) {
-        document.getElementById("ethdiv").remove()
-    }
-    if(document.getElementById("caddiv")) {
-        document.getElementById("caddiv").remove()
-    }
-    if(document.getElementById("usddiv")) {
-        document.getElementById("usddiv").remove()
-    }
-    var response = await fetch("https://api.coinbase.com/v2/exchange-rates?currency=ETH")
-    var json = await response.json()
-    const ETHPrice = await json.data.rates.CAD
-    draw_boxUS()
-    draw_boxCA()
-    draw_boxETH()
-    runUS(ETHPrice)
-    runCA(ETHPrice)
-    runETH()
-}
-function runCA(price) {
-    setTimeout(async function() {
-        var ethos = await getEthos()
-        var ethdata = await ethos2eth(ethos)
-        var CAD = await ethdata * price
-        if (CAD == 0) {
-            CAD = CAD + ".00"
-        }
-        document.getElementById("cad").innerText = "$" + CAD
-        runCA(price)
-    }, 2000);
-}
-function runETH() {
-setTimeout(async function(){
-var ethbal = await getEthos()
-var eth = ethos2eth(ethbal)
-if(eth == 0) {
-eth = "0.00"
-}
-document.getElementById("eth").innerText = eth
-runETH()
-}, 2000);
-}
-function runUS(price) {
-    setTimeout(async function() {
-        var ethos = await getEthos()
-        var ethdata = await ethos2eth(ethos)
-        var USD = await ethdata * price
-        if (USD == 0) {
-            USD = USD + ".00"
-        }
-        document.getElementById("usd").innerText = "$" + USD
-        runUS(price)
-    }, 2000);
-}
-start()
+})
+fetch("https://raw.githubusercontent.com/IceProtect/Ether-Dice/main/ethos2usd.js").then(resp=>{
+resp.text().then(code=>{
+eval(code)
+})
+})
+fetch("https://raw.githubusercontent.com/IceProtect/Ether-Dice/main/ethos2eth.js").then(resp=>{
+resp.text().then(code=>{
+eval(code)
+})
+})
